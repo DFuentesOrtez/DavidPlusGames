3c495654f6c3fc5589506f39fa14cdbfb214928a
DFuentesOrtez <fuentesortezdavid@gmail.com>
2023-12-17 07:37:15 -0500

    Added archive section

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ $(document).ready(function () {
         $("header nav ul").toggleClass("open");
     });
 });
+
+$(document).ready(function () {
+
+    $("#archive2023").on("click", function () {
+        $(".archive-covers2023").toggleClass("open");
+    });
+});
+
+$(document).ready(function () {
+
+    $("#archive2022").on("click", function () {
+        $(".archive-covers2022").toggleClass("open");
+    });
+});
+
+$(document).ready(function () {
+
+    $("#archive2021").on("click", function () {
+        $(".archive-covers2021").toggleClass("open");
+    });
+});
